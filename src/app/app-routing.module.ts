168b9a804a6dc870ad2c859df35e9f1b9d84375a
Marleen <mroubos2@avans.nl>
2021-03-20 15:32:45 +0100

    Routing -> About-books-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent} from './books/books.component';
+import { AboutMeComponent} from './about-me/about-me.component';
+import { HomePageComponent} from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'books-component', component: BooksComponent},
+  {path: 'about-component', component: AboutMeComponent},
+  {path: 'home-component', component: HomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    AboutMeComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
